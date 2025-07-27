94f8600c11d48be166dad27dd00e8a5d1f53f927
Joven <194885329+joven-l@users.noreply.github.com>
2025-07-27 20:20:35 +0800

    made structures in header for a rough idea of how to approach this and basic .c files

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 16:36:00 by joloo             #+#    #+#             */
-/*   Updated: 2025/07/22 16:36:32 by joloo            ###   ########.fr       */
+/*   Updated: 2025/07/27 20:19:16 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,47 @@
 # define MINISHELL_H
 
 #include "../libft/includes/libft.h"
+
+typedef	enum e_type
+{
+	
+}	t_type;
+
+typedef struct s_cmd
+{
+	char			*path;
+	char			**args;
+	char			**envp;
+	int				infile;
+	int				outfile;
+	int				stdout;
+	struct s_cmd	*next;
+}	t_cmd;
+
+typedef struct s_token
+{
+	char			*content;
+	t_type			type;
+	struct s_token	*next;
+}	t_token;
+
+typedef struct s_data
+{
+	char	**envp;
+	pid_t	*pid;
+	int		**pipes;
+	char	*input;
+	t_cmd	*cmd;
+	t_token	*token;
+}	t_data;
+
+// free_exit.c
+void	free_exit(t_data *data, int exit_code);
+
+// init.c
+void	init(t_data *data, char **envp);
+
+// start.c
+void	start(t_data *data)
 
 #endif
--- src/minishell.c
+++ src/minishell.c
@@ -6,13 +6,19 @@
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 16:36:43 by joloo             #+#    #+#             */
-/*   Updated: 2025/07/22 16:37:02 by joloo            ###   ########.fr       */
+/*   Updated: 2025/07/27 20:00:34 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
-	
+	t_data *data;
+
+	(void)argc;
+	(void)argv;
+	init(&data, envp);
+	start(&data);
+	free_exit(&data, 0);
 }
